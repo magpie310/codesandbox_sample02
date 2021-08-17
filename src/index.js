1b45168242a874a710ecebcd47189be693841250
magpie310 <51537530+magpie310@users.noreply.github.com>
2021-08-17 13:09:51 +0900

    中間提出 未完成

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./styles.css";
+// 追加ボタンクリック
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  // div作成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // li タグ
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+  // alert(inputText);
+
+  // ボタン(完了)を作成する
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  // ボタン(削除)を作成する
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  //
+};
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
